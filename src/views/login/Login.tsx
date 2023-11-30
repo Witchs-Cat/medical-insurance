443a0fb5232ae302639a4d3d888b08cf383759ba
Witchs-Cat <da18.06.03@yandex.ru>
2023-11-30 16:26:31 +0300

    сделана фомра регистрации

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import './login.css'
 
-const Login = () => {
+import {publicRoute} from '../../components/routing/PublicRoutes/PublicRoutes'
+
+
+const Login : React.FC = () => {
   return <main className='login'>
       <form className="login-form">
-        <input type="email" />
-        <input type="password" />
-        <button type="submit">Войти</button>
+        <div className="login-form__header">
+          <h2 className='login-form__header__title'>C Возвращением!</h2>
+          <p className='login-form__header__subtitle'>Мы рады видеть вас вновь!</p>
+        </div>
+
+        <div className='login-form__container'>
+          <label className="login-form__label" htmlFor="email">АДРЕС ЭЛЕКТРОНОЙ ПОЧТЫ</label>
+          <input className="login-form__input" type="email" />
+          <label className="login-form__label" htmlFor="password">ПАРОЛЬ</label>
+          <input className="login-form__input" type="password" />
+          <button className='login-form__button' type="submit">Войти</button>
+        </div> 
+
+        <div className='login__need-account'>
+        <span>Нужен новый аккаунт? </span>
+          <Link to={publicRoute.signup}>
+            зарегистрироваться
+          </Link>
+        </div>
+
       </form>
   </main>
 }
--- src/views/signup/Signup.tsx
+++ src/views/signup/Signup.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../login/Login'
+
+import {publicRoute} from '../../components/routing/PublicRoutes/PublicRoutes'
+
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+  return <main className='login'>
+      <form className="login-form">
+        <div className="login-form__header">
+          <h2 className='login-form__header__title'>Создайте учетную запись!</h2>
+        </div>
+        <div className='login-form__container'>
+          <label className="login-form__label" htmlFor="email">АДРЕС ЭЛЕКТРОНОЙ ПОЧТЫ</label>
+          <input className="login-form__input" type="email" />
+          <label className="login-form__label" htmlFor="email">Фамилия</label>
+          <input className="login-form__input" type="text" />
+          <label className="login-form__label" htmlFor="email">Имя</label>
+          <input className="login-form__input" type="text" />
+          <label className="login-form__label" htmlFor="email">Отчество</label>
+          <input className="login-form__input" type="text" />
+          <label className="login-form__label" htmlFor="email">Дата рождения</label>
+          <input className="login-form__input" type="text" />
+          <label className="login-form__label" htmlFor="password">ПАРОЛЬ</label>
+          <input className="login-form__input" type="password" />
+          <button className='login-form__button' type="submit">Зарегистрироваться</button>
+        </div> 
+        <div className='login__need-account'>
+        <span>Уже есть аккаунт? </span>
+          <Link to={publicRoute.login}>
+            Войти
+          </Link>
+        </div>
+
+      </form>
+  </main>
 }
 
 export default Signup
